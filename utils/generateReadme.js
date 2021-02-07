8352d95dbf5b751b66def969b3ac5822cecdd353
AverieBeltran <averiebeltran@github.com>
2021-02-07 14:07:53 -0600

    add license badge

--- utils/generateReadme.js
+++ utils/generateReadme.js
@@ -1,19 +1,9 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
 	return `
 		<h1 align="center">${data.title}</h1>
+
+		![badge](https://img.shields.io/badge/license-${data.license}-brightgreen)
 
 		## Table of Contents
 		- [Description] (#description)
@@ -40,10 +30,13 @@ function generateMarkdown(data) {
 		${data.tests}
 
 		## License
-		${data.license}
+		![badge](https://img.shields.io/badge/license-${data.license}-brightgreen)
+		<br />
+		This application by the ${data.license} license.
 
 		## Contact
 		:octocat: Find me on GitHub: [${data.username}](https://github.com/${data.username})
+		<br />
 		Email me with questions: ${data.email}
 	`;
 }
