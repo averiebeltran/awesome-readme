207bba9a580b015064d8f6d7910b992522924a48
AverieBeltran <averiebeltran@github.com>
2021-02-07 14:40:26 -0600

    added more badges

--- utils/generateReadme.js
+++ utils/generateReadme.js
@@ -3,6 +3,9 @@ function generateMarkdown(data) {
 	return `
 		<h1 align="center">${data.title}</h1>
 
+		![Badge for GitHub repo top language](https://img.shields.io/github/languages/top/averiebeltran/awesome-readme?style=flat&logo=appveyor)
+		![Badge for GitHub last commit](https://img.shields.io/github/last-commit/averiebeltran/awesome-readme?style=flat&logo=appveyor)
+
 		![badge](https://img.shields.io/badge/license-${data.license}-brightgreen)
 
 		## Table of Contents
